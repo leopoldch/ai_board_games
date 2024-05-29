0f3bd1f747e6a68dc5b473a61d3c5785da0082ce
Leo <chappuisleopold@gmail.com>
2024-05-29 14:21:40 +0200

    update conditions

--- gopher/gopher.py
+++ gopher/gopher.py
@@ -1,17 +1,23 @@
 """définition des fonctions de la classe du jeu gopher"""
 import math
 import random
+import time
 from utils import *
 
 class Gopher_Game:
     """classe du jeu gopher"""
 
-    def __init__(self,size:int) -> None:
+    def __init__(self,size:int, starting_player : Player) -> None:
         self.size = size
         self.firstmove = True
+        self.current_player = starting_player
         self.grid : Grid
         self.create_board()
     
+    def set_player(self,player:Player):
+        if player not in [1,2]:raise ValueError("Le joueur doit être soit 1 soit 2")
+        self.current_player = player
+
     def create_board(self) -> None:
         """create board using a size"""
         sizet = self.size * 2 - 1
@@ -114,7 +120,7 @@ class Gopher_Game:
                             neighbors.append(key)
         return neighbors
 
-    def is_legit(self, start: Cell, player: Player) -> bool:
+    def is_legit(self, start: Cell) -> bool:
         """returns if move is legit or not"""  
         if self.grid[start] != 0:
             return False
@@ -123,56 +129,49 @@ class Gopher_Game:
         neighbors: list[Cell] = self.get_neighbors(start[0], start[1])
         verif: int = 0
         for item in neighbors:
-            if self.grid[(item[0], item[1])] == player:
+            if self.grid[(item[0], item[1])] == self.current_player:
                 return False
-            if player == 1:
+            if self.current_player == 1:
                 if self.grid[(item[0], item[1])] == 2:
                     verif += 1
-            elif player == 2:
+            elif self.current_player == 2:
                 if self.grid[(item[0], item[1])] == 1:
                     verif += 1
         if verif == 1:
             return True
         return False
 
-    def legit_moves(self, player: Player) -> list[Cell]:
+    def legit_moves(self) -> list[Cell]:
         """returns legit moves in game"""
         results: list[Cell] = []
         if self.firstmove:return list(self.grid.keys())
         for item in self.grid.items():
-            if item[1] == 0 and self.is_legit(item[0], player):
+            if item[1] == 0 and self.is_legit(item[0]):
                 results.append(item[0])
         return results
 
-    def move(self, cell: Cell, player: Player) -> None:
+    def move(self, cell: Cell) -> None:
         """play item on grid"""
-        if not self.is_legit(cell, player):
+        if not self.is_legit(cell):
             raise ValueError("Impossible de bouger ce pion à cet endroit")
         else:
             if self.firstmove:self.firstmove = False
-            self.grid[cell] = player
+            self.grid[cell] = self.current_player
     
-    def score(self, player: Player) -> float:
+    def score(self) -> float:
         """evaluation func"""
         if self.firstmove:
             return 1
-        if player == 1:
-            if self.legit_moves(1) == []:
-                return -1
-            else:
-                return 1
-        elif player == 2:
-            if self.legit_moves(2) == []:
-                return -1
-            else:
-                return 1
+        if self.legit_moves() == []:
+            return -1
+        else:
+            return 1
 
     def final(self) -> bool:
         """returns if game has ended"""
-        if self.score(1) == 1 and self.score(2) == 1:
-            return True
-            
-        return False
+        if self.score() == -1:
+            return False
+        return True
     
     def minmax_action(self, player: Player, depth: int = 0) -> tuple[float, Action]:
         """minmax function"""
@@ -201,10 +200,9 @@ class Gopher_Game:
             return best
         raise ValueError("erreur pas de joeur connu")
 
-    def strategy_random(self, player: Player) -> tuple[Environment, Action]:
+    def strategy_random(self) -> tuple[Environment, Action]:
         """function to play with a random strat"""
-        legits: list[Cell] = self.legit_moves(player)
-        print(legits)
+        legits: list[Cell] = self.legit_moves()
         value = random.randint(0,len(legits)-1)
         return ({}, legits[value])
 
@@ -215,16 +213,34 @@ class Gopher_Game:
         return grid_to_state(self.grid)
 
 
-def test():
-    game = Gopher_Game(7)
-    cur_player : Player = 1
-    while game.final():
-        play : Action = game.strategy_random(cur_player)[1]
-        game.move(play,cur_player)
-        if cur_player ==1:cur_player=2
-        else:cur_player=1
-        print(game)
-        print(game.final())
-    print(game.final())
+def test(iter:int,size:int) ->None:
+    score : int =0
+    tps1 = time.time()
+    for _ in range(iter):
+        game = Gopher_Game(size=size,starting_player=1)
+        while game.final():
+            play : Action = game.strategy_random()[1]
+            game.move(play)
+            if game.current_player==1:game.set_player(player=2)
+            else:game.set_player(player=1)
+
+        #print(game)
+        # on compte le nombre de parties gagnées par le joueur 1
+
+        if game.current_player == 1:
+            if game.score() == 1:score+=1
+        else:
+            if game.score() == -1:score+=1
+        del game
         
-test()
+    print(
+        f"Temps d'éxécution pour {iter} itérations : {time.time() - tps1:.4f} secondes"
+    )
+    print(
+        f"Nombre de parties gagnées pour le joueur 1: {score} {(score/iter)*100:.2f}%"
+    )
+    print(
+        f"Nombre de parties gagnées pour le joueur 2: {iter-score} {((iter-score)/iter)*100:.2f}%"
+    )
+    
+test(100,7)
