89cdeae39c3eb6fac3f396e2ac6503f8d2f56cc6
leo <leopold@lchappuis.fr>
2024-06-10 19:37:18 +0200

    pylint test func

--- gopher/test.py
+++ gopher/test.py
@@ -1,24 +1,33 @@
-from gopher import *
+"""Fichier de test"""
 import time
+import math
+from gopher import GopherGame
+from utils import (
+    clear,
+    Action,
+    Player,
+)
 
-def test(iter: int, size: int,starting : Player) -> None:
+
+def test(iterations: int, size: int, starting: Player) -> None:
+    """fonction de test"""
     score: int = 0
     tps1 = time.time()
-    for i in range(iter):
-        if iter > 1:
+    for i in range(iterations):
+        if iterations > 1:
             clear()
-            print(f"Avancement : ", end=" ")
-            compteur: int = math.ceil((i / iter) * 100)
+            print("Avancement : ", end=" ")
+            compteur: int = math.ceil((i / iterations) * 100)
             print("[" + compteur * "-" + ((100 - compteur) * " " + "]"))
         game = GopherGame(size=size, starting_player=starting)
         while game.final():
-            #print(game)
+            print(game)
             if game.get_player() == 1:
-                play: Action = game.strategy_alpha_beta()
+                play: Action = game.strategy_random()
                 game.make_move(play)
                 game.set_player(player=2)
             else:
-                play: Action = game.strategy_random()
+                play: Action = game.strategy_negamax()
                 game.make_move(play)
                 game.set_player(player=1)
         # on compte le nombre de parties gagnées par le joueur 1
@@ -34,29 +43,27 @@ def test(iter: int, size: int,starting : Player) -> None:
 
     temps: float = time.time() - tps1
     print()
-    print(
-        f"Nombre d'itérations : {iter} | Taille de la grille : {size} | joueur de départ : {starting}"
-    )
+    print(f"Nb itérations : {iterations} | taille : {size} | joueur départ : {starting}")
     print(f"Temps d'éxécution  : {temps:.4f} secondes")
-    if iter > 1:
-        print(f"Temps par partie  : {temps/iter:.4f} secondes")
+    if iterations > 1:
+        print(f"Temps par partie  : {temps/iterations:.4f} secondes")
     print(
-        f"Nombre de parties gagnées pour le joueur 1: {score} {(score/iter)*100:.2f}%"
+        f"Nb de win pour le joueur 1: {score} {(score/iterations)*100:.2f}%"
     )
     print(
-        f"Nombre de parties gagnées pour le joueur 2: {iter-score} {((iter-score)/iter)*100:.2f}%"
+        f"Nb de win pour le joueur 2: {iterations-score} {((iterations-score)/iterations)*100:.2f}%"
     )
-
 
 
 def debug():
-    size = 3 
+    """fonction de debug"""
+    size = 3
     for i in range(7):
-        game = GopherGame(size=size+i)
+        game = GopherGame(size=size + i)
         print(game)
         del game
 
 
-test(iter=1, size=6,starting=1)
+test(iterations=10, size=6, starting=1)
 
-#debug()
+# debug()
